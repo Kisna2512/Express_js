d63d0a45cb96007ef0c54354e3bd7676f043b406
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2023-02-28 07:49:24 +0000

    routes

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router=express.Router()
+
+
+
+router.get('/', (req, res) => {
+    res.send("users list")
+})
+
+router.get('/new', (req, res) => {
+    res.send("users new form")
+})
+
+
+module.exports=router
